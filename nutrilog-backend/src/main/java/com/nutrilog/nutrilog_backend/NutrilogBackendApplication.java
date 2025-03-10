d5cc10cf34027caa8aff4daea7b00f1e58ba1262
devRonPark <pbc9236@gmail.com>
2025-03-10 15:32:08 +0900

    chore: 프로젝트 구성 및 기본 의존성 추가 #1

---
+++ nutrilog-backend/src/main/java/com/nutrilog/nutrilog_backend/NutrilogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nutrilog.nutrilog_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrilogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrilogBackendApplication.class, args);
+	}
+
+}
---
+++ nutrilog-backend/src/test/java/com/nutrilog/nutrilog_backend/NutrilogBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrilog.nutrilog_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrilogBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
