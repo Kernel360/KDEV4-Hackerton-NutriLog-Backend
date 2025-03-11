0b3c6f4eebdbab78d8b3857116014f5f865420f6
Tojaman <zzoon5020@naver.com>
2025-03-11 11:27:23 +0900

    feat: 영양제 스케줄 등록 API

---
+++ nutrilog-backend/src/main/java/com/nutrilog/nutrilog_backend/supplement/Status.java
@@ -0,0 +1,6 @@
+package com.nutrilog.nutrilog_backend.supplement;
+
+public enum Status {
+    TAKEN,
+    UNTAKEN
+}
---
+++ nutrilog-backend/src/main/java/com/nutrilog/nutrilog_backend/supplement/repository/SupplementSchduleRepository.java
@@ -0,0 +1,15 @@
+package com.nutrilog.nutrilog_backend.supplement.repository;
+
+import java.time.DayOfWeek;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nutrilog.nutrilog_backend.supplement.entity.SupplementSchedule;
+
+@Repository
+public interface SupplementSchduleRepository extends JpaRepository<SupplementSchedule, Long> {
+    
+    // public List<SupplementSchedule> findByDayOfWeek(DayOfWeek dayOfWeek);
+}
