6a8691deb430273edf33149880f6a0ae60cceb79
devRonPark <pbc9236@gmail.com>
2025-03-11 11:34:27 +0900

    feat: 액세스 토큰 검증하는 필터 구현 및 적용

--- nutrilog-backend/src/main/java/com/nutrilog/nutrilog_backend/auth/controller/AuthController.java
+++ nutrilog-backend/src/main/java/com/nutrilog/nutrilog_backend/auth/controller/AuthController.java
@@ -4,6 +4,7 @@ import com.nutrilog.nutrilog_backend.auth.dto.LoginRequest;
 import com.nutrilog.nutrilog_backend.auth.dto.LoginResponse;
 import com.nutrilog.nutrilog_backend.auth.dto.OAuthUserInfo;
 import com.nutrilog.nutrilog_backend.auth.service.AuthService;
+import com.nutrilog.nutrilog_backend.common.entities.User;
 import com.nutrilog.nutrilog_backend.common.entities.UserType;
 import com.nutrilog.nutrilog_backend.common.util.NicknameGenerator;
 import lombok.RequiredArgsConstructor;
@@ -11,10 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -30,5 +29,11 @@ public class AuthController {
         OAuthUserInfo oAuthUserInfo = authService.getOAuthUserInfo(loginRequest.getCode(), loginRequest.getProvider());
         LoginResponse response = authService.login(oAuthUserInfo.getSocialKey(), oAuthUserInfo.getProvider(), loginRequest.getCode());
         return response;
+    }
+
+    // 테스트용 메서드
+    @GetMapping("/token-info")
+    public User getTokenInfo(@AuthenticationPrincipal User userDetails) {
+        return userDetails;
     }
 }
--- nutrilog-backend/src/main/java/com/nutrilog/nutrilog_backend/auth/service/impl/TokenServiceImpl.java
+++ nutrilog-backend/src/main/java/com/nutrilog/nutrilog_backend/auth/service/impl/TokenServiceImpl.java
@@ -3,11 +3,15 @@ package com.nutrilog.nutrilog_backend.auth.service.impl;
 import com.nutrilog.nutrilog_backend.auth.dto.JwtProperties;
 import com.nutrilog.nutrilog_backend.auth.service.TokenService;
 import com.nutrilog.nutrilog_backend.common.entities.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -17,6 +21,7 @@ import java.util.Date;
 @Service
 @RequiredArgsConstructor
 public class TokenServiceImpl implements TokenService {
+    private final UserDetailsService userDetailsService;
     private final JwtProperties jwtProperties;
 
     // JWT Access 토큰 생성 메소드
@@ -36,6 +41,7 @@ public class TokenServiceImpl implements TokenService {
                 .setExpiration(expiredDate) // 만료 시간 설정
                 .setSubject(user.getNickname()) // 토큰 주제(사용자 닉네임)
                 .claim("id", user.getId()) // 사용자 ID claim 설정
+                .claim("socialKey", user.getSocialKey())
                 .signWith(getSecretKey(), SignatureAlgorithm.HS256) // 비밀키와 해시 알고리즘 설정
                 .compact(); // 최종적으로 토큰 압축하여 문자열 반환
 
@@ -47,4 +53,53 @@ public class TokenServiceImpl implements TokenService {
     private SecretKey getSecretKey() {
         return Keys.hmacShaKeyFor(jwtProperties.getSecretKey().getBytes());
     }
+
+    // 토큰이 유효한지 검증 메소드
+    public boolean validateToken(String token) {
+        log.info("[validateToken] 토큰 검증을 시작합니다.");
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSecretKey()) // 비밀키 설정
+                    .build()
+                    .parseClaimsJws(token); // 서명된 클레임을 파싱
+
+            log.info("토큰 검증 통과");
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        log.info("토큰 검증 실패");
+        return false;
+    }
+
+    // 토큰에서 정보(Claim) 추출 메소드
+    private Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSecretKey()) // 비밀키 설정
+                .build()
+                .parseClaimsJws(token) // JWT 파싱 (유효성 검사 포함)
+                .getBody(); // 클레임 정보 반환
+    }
+
+
+    // 토큰에서 인증 정보 반환하는 메소드
+    public Authentication getAuthenticationByToken(String token) {
+        log.info("[getAuthenticationByToken] 토큰 인증 정보 조회");
+        String socialKey = getSocialKeyByToken(token);
+        User user = (User) userDetailsService.loadUserByUsername(socialKey);
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                user, token, null
+        );
+        return authentication;
+    }
+
+    // 토큰에서 사용자 이메일만 추출하는 메소드
+    public String getSocialKeyByToken(String token) {
+        log.info("[getSocialKeyByToken] 토큰 기반 회원 식별 정보 추출");
+        Claims claims = getClaims(token);
+        log.info("{}", claims.get("socialKey", String.class));
+        String socialKey = claims.get("socialKey", String.class);
+        return socialKey;
+    }
+
 }
